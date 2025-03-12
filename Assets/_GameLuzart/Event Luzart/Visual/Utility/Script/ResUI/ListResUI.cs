d8e24cb1ce41548e546a1409205ec751b63361b8
luzartanh <anhmd@blackgemsglobal.com>
2025-03-12 11:26:03 +0700

    .

--- Assets/_GameLuzart/Event Luzart/Visual/Utility/Script/ResUI/ListResUI.cs
+++ Assets/_GameLuzart/Event Luzart/Visual/Utility/Script/ResUI/ListResUI.cs
@@ -7,7 +7,19 @@ namespace Luzart
     public class ListResUI : MonoBehaviour
     {
         public ResUI resUIPf;
-        public Transform parentSpawn;
+        [SerializeField]
+        private Transform _parentSpawn = null;
+        public Transform parentSpawn
+        {
+            get
+            {
+                if (_parentSpawn == null)
+                {
+                    _parentSpawn = transform;
+                }
+                return _parentSpawn;
+            }
+        }
         public List<ResUI> listResUI = new List<ResUI>();
         public void InitResUI(params DataResource[] dataResources)
         {
